c942ae4d85d9e011a5d42cbd0ebceebce1b4e57c
muhammadsarim <muhammadsarim>
2026-01-07 19:23:56 +0500

    update backend settings

--- db/db.connection.js
+++ db/db.connection.js
@@ -1,33 +1,4 @@
-// const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
 
-// dotenv.config();
-
-
-// async function dbCon() {
-
-//     try {
-
-//         const db = await mongoose.connect(process.env.db)
-//             .then(() => console.log("Database Connected"))
-//             .catch((err) => console.log(`Connection Faild ${err}`))
-
-//         mongoose.connection.on('connected', () =>
-//             console.log('DATABASE SUCCESSFULLY CONNECTED...!')
-//         );
-
-//         mongoose.connection.on('disconnected', () =>
-//             console.log('DATABASE CONNECTION TERMINATED...!')
-//         );
-
-//     }
-//     catch (err) {
-//         console.log(err, "here in an error");
-//     }
-// }
-
-
-// module.exports = dbCon;
 
 const mongoose = require("mongoose");
 
@@ -38,7 +9,7 @@ const dbCon = async () => {
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error("❌ MongoDB Connection Failed:", error.message);
-    process.exit(1); // 👈 VERY IMPORTANT (prevents 502)
+    process.exit(1); 
   }
 };
 
